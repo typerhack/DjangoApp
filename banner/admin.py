5e6268907b76d67989e0cf5462cb249705a10195
Daniel <daniel.dolatabadi@gmail.com>
2020-04-27 17:20:32 +0430

    Changing admin for banner

--- banner/admin.py
+++ banner/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import Banner
 
 # Register your models here.
-admin.site.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project_name', 'add_date', 'is_published')
+    list_display_links = ('id','project_name')
+    list_filter = ('is_published',)
+    list_editable = ('is_published',)
+    search_fields = ('project_name',)
+    list_per_page = 25
+admin.site.register(Banner, BannerAdmin)
