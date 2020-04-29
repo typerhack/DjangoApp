8d905c48016e977faf71401e105ce150a3328269
Daniel <daniel.dolatabadi@gmail.com>
2020-04-29 14:06:06 +0430

    Post Blog before persian date

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from author.models import Author
+
 
 # Create your views here.
 def index(request):
-    blog=Blog.objects.order_by('-article_date').filter(is_published=True)
-    paginator = Paginator(blog,12)
+    blog = Blog.objects.order_by('-article_date').filter(is_published=True)
+    paginator = Paginator(blog, 12)
     page = request.GET.get('page')
     paged_blog = paginator.get_page(page)
-    context={
-        'blog':paged_blog
+    context = {
+        'blog': paged_blog
     }
-    return render(request,'blog/posts.html',context)
+    return render(request, 'blog/posts.html', context)
+
 
 def blog(request, post_id):
-    return render(request,'blog/post.html')
+    blog_post = get_object_or_404(Blog, pk=post_id)
+
+    context = {
+        'blog_post': blog_post,
+    }
+
+    return render(request, 'blog/post.html', context)
+
 
 def search(request):
-    return render(request,'blog/search.html')
+    return render(request, 'blog/search.html')
